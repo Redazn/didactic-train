6467ae3076944d18c188b4bb7c2673719d8507ef
Redazn <146597521+Redazn@users.noreply.github.com>
2025-06-03 00:25:06 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="cognitive_framework",
+    name="dictactic-train",
     version="0.1.0",
     packages=find_packages(),
     install_requires=[
